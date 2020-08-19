cc1b19e2c5e53758c2043311754c0d2a1f12991a
kimsutto <suginkim1@gmail.com>
2020-08-19 23:50:53 +0900

    feat : Add Login

---
+++ src/main/java/com/kimsutto/escaperoom/model/param/LoginParam.java
@@ -0,0 +1,12 @@
+package com.kimsutto.escaperoom.model.param;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class LoginParam {
+  String token;
+}
